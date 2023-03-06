76c7a1662fa34a3024fbf1c57b0003bd6fa74f62
taras <taras.fid@gmail.com>
2023-03-06 22:08:25 +0200

    added img to the main and cart pages.

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from cart.models import Product, Order, Category
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(Category)
--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 
 class Category(models.Model):
@@ -15,6 +16,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='app/static/images/products_img',
+                              default='static/images/products_img/default.jpg')
 
     def __str__(self):
         return self.name
